d8dce463957c1b032d32d117af94fb800be5d306
Hedgestock <arthur.pamart@epita.fr>
2020-05-17 22:58:37 +0200

    Removed led 3 from layer rotation since it is now used for caps lock and moved some keys around

--- ergodox_ez_aito-azerty/config.h
+++ ergodox_ez_aito-azerty/config.h
@@ -3,4 +3,9 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define FIRMWARE_VERSION u8"9wGj7/GWGbj"
+#define PERMISSIVE_HOLD
+#define FORCE_NKRO
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#define FIRMWARE_VERSION u8"9wGj7/Wb473"
--- ergodox_ez_aito-azerty/keymap.c
+++ ergodox_ez_aito-azerty/keymap.c
@@ -32,37 +32,37 @@
 #define NO_BSLS_ALT KC_EQUAL
 #define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
 
-enum custom_keycodes {
-  ALT_0198 = SAFE_RANGE
+enum my_keycodes {
+  XMAS1 = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      FR_AMP,         FR_EACU,        FR_DQUO,        FR_APOS,        FR_LPRN,        TT(2),                                          TT(2),          FR_MINS,        FR_EGRV,        FR_UNDS,        FR_CCED,        FR_AGRV,        KC_INSERT,
     KC_TAB,         FR_A,           FR_Z,           KC_E,           KC_R,           KC_T,           FR_ASTR,                                        FR_DLR,         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           FR_CIRC,
-    KC_LSHIFT,      FR_Q,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           FR_M,           KC_RSHIFT,
+    LSFT_T(KC_CAPSLOCK),FR_Q,       KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           FR_M,           RSFT_T(KC_CAPSLOCK),
     KC_LCTRL,       FR_W,           KC_X,           KC_C,           KC_V,           KC_B,           KC_APPLICATION,                                 TT(1),          KC_N,           FR_COMM,        FR_SCLN,        FR_COLN,        FR_EXLM,        KC_RCTRL,
-    KC_CAPSLOCK,    KC_LALT,        KC_LGUI,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LGUI,        KC_RALT,        FR_UGRV,
+    FR_LESS,        KC_LALT,        KC_LGUI,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LGUI,        KC_RALT,        FR_UGRV,
                                                                                                     KC_HOME,        KC_END,         KC_HOME,        KC_END,
-                                                                                                                    FR_LESS,        KC_PGUP,
+                                                                                                                    KC_ENTER,       KC_PGUP,
                                                                                     KC_BSPACE,      KC_DELETE,      TT(1),          KC_PGDOWN,      KC_ENTER,       KC_SPACE
   ),
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_TRANSPARENT, FR_LESS,        LSFT(FR_LESS),  RALT(FR_APOS),  RALT(FR_EQL),   RALT(FR_MINS),  RALT(FR_UNDS),                                  KC_TRANSPARENT, KC_KP_MINUS,    KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_ASTERISK, KC_F12,
-    RALT(FR_EGRV),  FR_DQUO,        FR_APOS,        FR_LPRN,        FR_RPRN,        FR_EQL,                                                                         KC_KP_PLUS,     KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_SLASH,    KC_TRANSPARENT,
-    KC_TRANSPARENT, RALT(FR_UNDS),  LSFT(FR_COLN),  RALT(FR_LPRN),  RALT(FR_RPRN),  FR_ASTR,        RALT(KC_0),                                     KC_TRANSPARENT, FR_COLN,        KC_KP_1,        KC_KP_2,        KC_KP_3,        LSFT(FR_UGRV),  KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_KP_0,        KC_KP_DOT,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, RALT(FR_EGRV),  RALT(FR_DQUO),  RALT(FR_APOS),  RALT(FR_EQL),   RALT(FR_MINS),  RALT(FR_UNDS),                                  KC_TRANSPARENT, KC_KP_MINUS,    KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_ASTERISK, KC_F12,
+    KC_TRANSPARENT, FR_LESS,        LSFT(FR_LESS),  FR_LPRN,        FR_RPRN,        FR_EQL,                                                                         KC_KP_PLUS,     KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_KP_SLASH,    KC_TRANSPARENT,
+    KC_TRANSPARENT, RALT(FR_UNDS),  LSFT(FR_COLN),  RALT(FR_LPRN),  RALT(FR_RPRN),  FR_SUP2,        RALT(KC_0),                                     KC_TRANSPARENT, FR_COLN,        KC_KP_1,        KC_KP_2,        KC_KP_3,        LSFT(FR_UGRV),  KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_KP_0,        KC_KP_DOT,      KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, LCTL(KC_S),     LCTL(FR_Z),     LCTL(KC_Y),     LCTL(LSFT(KC_ESCAPE)),WEBUSB_PAIR,                              KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, LCTL(FR_A),     LCTL(KC_X),     LCTL(KC_C),     LCTL(KC_V),     LGUI(LSFT(KC_S)),                                                               KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_TRANSPARENT,                           KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, LCTL(FR_A),     LCTL(KC_S),     LCTL(FR_Z),     LCTL(KC_Y),     LCTL(LSFT(KC_ESCAPE)),WEBUSB_PAIR,                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, LED_LEVEL,      LCTL(KC_X),     LCTL(KC_C),     LCTL(KC_V),     LGUI(LSFT(KC_S)),                                                                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,                                                                                                KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
+/*
 void alt_code(char *code) { 
   SEND_STRING(SS_DOWN(X_LALT));
 
@@ -122,6 +122,7 @@ void alt_code(char *code) {
   SEND_STRING(SS_UP(X_LALT));
 }
 
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case ALT_0198:
@@ -132,14 +133,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+*/
 
-
-void led_set_user(uint8_t usb_led) {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+bool led_update_user(led_t led_state) {
+    if (led_state.caps_lock) {
         ergodox_right_led_3_on();
     } else {
         ergodox_right_led_3_off();
     }
+    return true;    
 }
 
 uint32_t layer_state_set_user(uint32_t state) {
@@ -147,7 +149,6 @@ uint32_t layer_state_set_user(uint32_t state) {
   ergodox_board_led_off();
   ergodox_right_led_1_off();
   ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
   switch (layer) {
     case 1:
       ergodox_right_led_1_on();
@@ -164,11 +165,4 @@ uint32_t layer_state_set_user(uint32_t state) {
   }
   return state;
 };
-
-// if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-//         ergodox_right_led_2_on();
-//     } else {
-//         ergodox_right_led_2_off();
-//     }
-/* Custom ALT + UNICODE */
 
